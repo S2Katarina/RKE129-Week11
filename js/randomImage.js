b2dd5fce09838c54878e391fd921e1fa9c18fc12
Katarina Suu <katarina.suu@tktk.ee>
2023-05-24 13:06:15 +0300

    Week11 tunni ja iseseisevtöö

---
+++ js/randomImage.js
@@ -0,0 +1,13 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["../images/pic1.jpg", "../images/pic2.jpg", "../images/pic3.jpg", "../images/pic4.jpg"];
+
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    let randomImage = images[randomIndex];
+    image.src = randomImage;
+    console.log("Uus pilt:", randomImage);
+}
